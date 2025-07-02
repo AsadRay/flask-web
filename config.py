88a2a132f9c9e835e11424f68c525724573d04ed
Your Name <youremail@example.com>
2025-07-02 11:06:44 +0600

    removed security key

--- config.py
+++ config.py
@@ -14,17 +14,3 @@ class Config:
     ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
     POSTS_PER_PAGE = 3
     LANGUAGES = ['en', 'es', 'bn']
-# class Config:
-#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-#         'sqlite:///' + os.path.join(basedir, 'app.db')
-
-#     MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.sendgrid.net'
-#     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
-#     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', '1') == '1'
-#     MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'apikey'   # your SendGrid username
-#     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or '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'  # your SendGrid API key
-#     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'wassemrayhan02@gmail.com'
-#     ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL') or 'asaduzzamanrayhan15@gmail.com'
-
-#     POSTS_PER_PAGE = 3
